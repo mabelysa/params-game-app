1a35e722bd1d59fdc2f48f01a7f64aeabce1ea95
Mabelys Arias <MabelysA@gmail.com>
2021-06-17 14:57:12 -0400

    adds segment

--- app/controllers/params_game_controller.rb
+++ app/controllers/params_game_controller.rb
@@ -1,2 +1,31 @@
 class ParamsGameController < ApplicationController
+  def name
+    input_value = params["message"]
+    if input_value.first == "a"
+      render json: { message: "Hey, your name starts with the first letter of the alphabet!" }
+    else
+      render json: { message: "You're not as special as you think you are!" }
+    end
+  end
+
+  def guessgame
+    winning_number = 32
+    input_guess = params["the_users_guess"].to_i
+    if input_guess > winning_number
+      output_message = "Pick lower!"
+    elsif input_guess < winning_number
+      output_message = "Pick higher!"
+    else 
+      output_message = "You win!"
+    end
+      render json {message: output_message}
+    end
+  end
+
+  def segment_params
+    input_value = params["this_is_a_key"]
+    render json: {message: "The url segment is #{input_value}"
+  end 
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/query_params" => "params_game#name"
+  get "/query_params" => "params_game#guessgame"
+
+  get "/segment_params/:this_is_a_key" => "params_game#segment_params"
+
+  get "/segment_guess/:the_users_guess" => "params_game#guessgame"
 end
